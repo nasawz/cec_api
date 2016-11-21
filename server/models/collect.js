20bd3b0722d395d5b0782499065d1553331d27a6
王喆 <nasa127@gmail.com>
2016-11-21 13:30:18 +0800

    remote-method 定义完成

--- server/models/collect.js
+++ server/models/collect.js
@@ -2,18 +2,71 @@
 // var app = require('../../server/server');
 module.exports = function(Collect) {
   /**
- *
- * @param {string} msg
- * @param {Function(Error, string, string)} callback
- */
-
-  Collect.greet = function(msg, callback) {
-    var app = Collect.app;
-
-    console.log(app.models)
-    var result = msg;
-    var hi = 'hi';
+   * 当前登录的所有者抽奖
+   * @param {string} cid collect id
+   * @param {Function(Error, object)} callback
+   */
+  Collect.lottery = function(cid, callback) {
+    var result = {
+      result: 'succ'
+    };
     // TODO
-    callback(null, result, hi);
+    callback(null, result);
   };
+
+  /**
+   * 支持
+   * @param {string} cid collect id
+   * @param {Function(Error, object)} callback
+   */
+  Collect.support = function(cid, callback) {
+    var result = {
+      result: 'succ'
+    };
+    // TODO
+    callback(null, result);
+  };
+
+  /**
+   * 允许
+   * @param {string} cid collect id
+   * @param {string} code 经销店编码
+   * @param {Function(Error, object)} callback
+   */
+  Collect.permit = function(cid, code, callback) {
+    var result = {
+      result: 'succ'
+    };
+    // TODO
+    callback(null, result);
+  };
+
+
+  /**
+   * 当前登录用户留资
+   * @param {string} cid collect id
+   * @param {object} data 联系人数据
+   * @param {Function(Error, object)} callback
+   */
+  Collect.contacts = function(cid, data, callback) {
+    var result = {
+      result: 'succ'
+    };
+    // TODO
+    callback(null, result);
+  };
+
+  /**
+   * 参与
+   * @param {string} channel 渠道
+   * @param {Function(Error, object)} callback
+   */
+  Collect.join = function(channel, callback) {
+    var result = {
+      result: 'succ'
+    };
+    // TODO
+    callback(null, result);
+  };
+
 };
--- server/models/seller.js
+++ server/models/seller.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Seller) {
-
+  /**
+   * 认证经销店
+   * @param {string} code 经销店编码
+   * @param {Function(Error, object)} callback
+   */
+  Seller.auth = function(code, callback) {
+    var result = {
+      result: 'succ'
+    };
+    // TODO
+    callback(null, result);
+  };
 };
