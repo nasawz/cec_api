cfe5dfa72d3741ca7928c8826430f0e48169c326
王喆 <nasa127@gmail.com>
2016-11-23 17:51:33 +0800

    (っ˘▽˘)っ nasa ⊂(◕。◕⊂)

--- server/models/tenant.js
+++ server/models/tenant.js
@@ -1,21 +1,21 @@
 'use strict';
 
 module.exports = function(Tenant) {
-  Seller.disableRemoteMethod('create', true); // Removes (POST) /module
+  Tenant.disableRemoteMethod('create', true); // Removes (POST) /module
 
-  Seller.disableRemoteMethod('find', true);
-  Seller.disableRemoteMethod('findById', true);
-  Seller.disableRemoteMethod('findOne', true);
-  Seller.disableRemoteMethod('count', true);
-  Seller.disableRemoteMethod('exists', true);
+  Tenant.disableRemoteMethod('find', true);
+  Tenant.disableRemoteMethod('findById', true);
+  Tenant.disableRemoteMethod('findOne', true);
+  Tenant.disableRemoteMethod('count', true);
+  Tenant.disableRemoteMethod('exists', true);
 
-  Seller.disableRemoteMethod('upsert', true); // Removes (PUT) /module
-  Seller.disableRemoteMethod('updateAll', true); // Removes (POST) /module/update
-  Seller.disableRemoteMethod('updateAttributes', false); // Removes (PUT) /module/:id
-  Seller.disableRemoteMethod('createChangeStream', true); // removes (GET|POST) /module/change-stream
-  Seller.disableRemoteMethod('upsertWithWhere', true);
-  Seller.disableRemoteMethod('replaceOrCreate', true);
-  Seller.disableRemoteMethod('replaceById', true);
+  Tenant.disableRemoteMethod('upsert', true); // Removes (PUT) /module
+  Tenant.disableRemoteMethod('updateAll', true); // Removes (POST) /module/update
+  Tenant.disableRemoteMethod('updateAttributes', false); // Removes (PUT) /module/:id
+  Tenant.disableRemoteMethod('createChangeStream', true); // removes (GET|POST) /module/change-stream
+  Tenant.disableRemoteMethod('upsertWithWhere', true);
+  Tenant.disableRemoteMethod('replaceOrCreate', true);
+  Tenant.disableRemoteMethod('replaceById', true);
 
-  Seller.disableRemoteMethod('deleteById', true); // Removes (DELETE) /module/:id
+  Tenant.disableRemoteMethod('deleteById', true); // Removes (DELETE) /module/:id
 };
