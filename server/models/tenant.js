4b8aea93741a280be3012fe88a2c072125b0a8d7
王喆 <nasa127@gmail.com>
2016-11-23 17:51:16 +0800

    (っ˘▽˘)っ nasa ⊂(◕。◕⊂)

--- server/models/tenant.js
+++ server/models/tenant.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Tenant) {
+  Seller.disableRemoteMethod('create', true); // Removes (POST) /module
 
+  Seller.disableRemoteMethod('find', true);
+  Seller.disableRemoteMethod('findById', true);
+  Seller.disableRemoteMethod('findOne', true);
+  Seller.disableRemoteMethod('count', true);
+  Seller.disableRemoteMethod('exists', true);
+
+  Seller.disableRemoteMethod('upsert', true); // Removes (PUT) /module
+  Seller.disableRemoteMethod('updateAll', true); // Removes (POST) /module/update
+  Seller.disableRemoteMethod('updateAttributes', false); // Removes (PUT) /module/:id
+  Seller.disableRemoteMethod('createChangeStream', true); // removes (GET|POST) /module/change-stream
+  Seller.disableRemoteMethod('upsertWithWhere', true);
+  Seller.disableRemoteMethod('replaceOrCreate', true);
+  Seller.disableRemoteMethod('replaceById', true);
+
+  Seller.disableRemoteMethod('deleteById', true); // Removes (DELETE) /module/:id
 };
